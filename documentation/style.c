8c40b4f40f8f1338e9fafb964fa48814a4250784
tomrbland <tomrbland@yahoo.co.uk>
2015-10-21 21:18:40 +0100

    Started a first draft of a style sheets. Rip it apart and debate please!

---
+++ documentation/style.c
@@ -0,0 +1,54 @@
+/*Please can we debate a style sheet so that we can all be coding in the same style?*/
+
+/*RULE 1: FOLLOW NEILS HOUSE STYLES*/
+
+/*I would vote to move to C99 so that we can use // comments.
+  However, I would avoid declerations of variables mid way
+  through a function (allowed in c99). As they encourage longer
+  functions. An excception to this would be for(int i = 0:......){}
+*/
+
+/*Functions should be short. No longer than a page*/
+
+void filename_function(void);
+/*
+    filename_ dictates that this function is accessable to those
+    that have included filename.h
+    This function should be as short and closed as possible to
+    maintain the scope of filename.c
+*/
+
+void filenameFunction(void);
+/*
+    camelCase shows function is only accessable within the scope of
+    filename.c
+    Does not need to include filename
+*/
+
+void function(void){
+  /*brace at end of line*/
+}
+
+if{
+
+}
+if else{
+
+}
+else{
+
+}
+/*tempted to move to:
+      if{
+
+      }
+      if else{
+
+      } else{
+
+      }
+*/
+
+for (i=0; i<x; i++){
+
+}
