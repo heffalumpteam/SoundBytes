e79ae11642e1dd6e278941db36f0aa9280ab17cb
Berk M <berkmolla@gmail.com>
2015-11-17 16:30:28 +0000

    Custom language file for language 'heffalump'. Updated graphics.c to read from that.

--- graphics.c
+++ graphics.c
@@ -36,7 +36,7 @@ void initSourceView(GtkBuilder *builder){
   GtkSourceView *sourceview = GTK_SOURCE_VIEW(gtk_builder_get_object (builder, "gtksourceview1"));
   GtkSourceBuffer *sourcebuffer = GTK_SOURCE_BUFFER(gtk_text_view_get_buffer(GTK_TEXT_VIEW(sourceview)));
   GtkSourceLanguageManager *manager = gtk_source_language_manager_get_default();
-  GtkSourceLanguage *python = gtk_source_language_manager_get_language(manager, "python");
+  GtkSourceLanguage *python = gtk_source_language_manager_get_language(manager, "heffalump");
   gtk_source_buffer_set_language(sourcebuffer, python);
 
   gtk_text_buffer_set_text(GTK_TEXT_BUFFER(sourcebuffer),
