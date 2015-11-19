e0200cedaf5c091ac0cb42de3ca0889a187700f8
Olly King <ok15351@my.bristol.ac.uk>
2015-11-19 17:50:51 +0000

    Initial program that splits function into parts on '.' or brackets

---
+++ translator/lost_in_translator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXWORD 50
+
+int main (void){
+
+   char input_string[MAXWORD];
+
+   printf("Type in a command: ");
+   scanf("%s", input_string);
+}
