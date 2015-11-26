6bb51b8b64e7613224798daf4348138673f55f4f
tomrbland <tomrbland@yahoo.co.uk>
2015-11-26 12:29:15 +0000

    lets get ready to push!

--- text.c
+++ text.c
@@ -7,45 +7,44 @@
 #include "text.h"
 #include "events.h"
 
-void text_recieveUpdate(char *s){
-  char* p, *add = "add", *stop = "stop";
+void addLoop(char *p);
+
+void text_receiveUpdate(char *s){
+  char* p;
   int i = 1;
 
 /*Takes input line from GUI and splits into individual words*/
   p = strtok(s, " \n.()");
   do{
     printf("Instruction %d Received: %s\n", i, p);
-    if(strcmp(p, add) == 0){
-      text_add(p);
+    if(strcmp(p, "add") == 0){
+      addLoop(p);
     }
-    if(strcmp(p, stop) == 0){
-      events_stop();
+    if(strcmp(p, "stop") == 0){
+      printf("Stop!\n");
     }
     p = strtok(NULL, " \n.()");
     i++;
   }while(p);
 }
 
-void text_add(char *p){
-  char *s, *drums = "drums", *clap = "clap";
+void addLoop(char *p){
+  char *s;
   p = strtok(NULL, " \n.()");
   if(p){
     printf("Add function: Instrument: %s\n", p);
-    if(strcmp(p, drums) == 0){
-/*For now start the loop, but when we have more loops, call each individually
-  depending on keyword*/
-      events_start(); 
+    if(strcmp(p, "drums") == 0){
+      printf("drums\n");
     }
-    if(strcmp(p, clap) == 0){
+    if(strcmp(p, "clap") == 0){
       printf("clap\n");
-      events_clap1();
     }
-/*If there are other intructions (on other lines) to carry out, send them back to text_recievUpdate
+/*If there are other intructions (  on other lines) to carry out, send them back to text_recievUpdate
 function to sort out*/
     s = strtok(NULL, "");
     if(s){
       printf("Passed back %s\n", s);
-      text_recieveUpdate(s);
+      text_receiveUpdate(s);
     }
   }
 }
