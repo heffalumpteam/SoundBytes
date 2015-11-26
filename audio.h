e09417c0817e1afa332092f265d9691e8db70af0
tomrbland <tomrbland@yahoo.co.uk>
2015-11-26 14:28:34 +0000

    Added Enum and function dec

--- audio.h
+++ audio.h
@@ -1,3 +1,22 @@
+enum loop{
+  DRUMS_SHUFFLE,
+  DRUMS_CLAP,
+  SYNTH_STAB
+};
+typedef enum loop Loop;
+
+enum instruction{
+  START, /*Starting playback*/
+  STOP,  /*Stop playback*/
+  ADD,   /*Add a loop*/
+  REMOVE /*Stop a loop*/
+};
+typedef enum instruction Instruction;
+
 void audio_init(void);
 void audio_close(void);
-void audio_playSampleOnce(char* sampleName);
+void audio_playSampleOnce(Loop index);
+void audio_startLoop(Loop index);
+void audio_stopLoop(Loop index);
+void audio_addLoop(Loop index);
+void audio_removeLoop(Loop index);
