3ebe309fd3aad7226e1ef8a027eef00c86104b9a
tomrbland <tomrbland@yahoo.co.uk>
2016-01-25 15:33:24 +0000

    Comments to audio.c

--- audio.c
+++ audio.c
@@ -18,13 +18,12 @@
 #include <limits.h>
 
 #define USE_OMP
+//#define NO_OF_CORES
+//#define DEBUG_TIMING
 
 #ifdef USE_OMP
   #include <omp.h>
 #endif
-
-//#define NO_OF_CORES
-//#define DEBUG_TIMING
 
 #define SAMPLERATE 44100
 #define NUMAUDIOCHANNELS 2
@@ -37,6 +36,7 @@
 #define DEFAULTCHANNEL INT_MAX
 #define BEATS_IN_A_BAR 4
 
+//All to aid readability of audio_mainLoop
 #define SAMPLE_IS_ACTIVE(i) activeSamples[i].active
 #define SET_VOLUME(i) Mix_Volume(activeSamples[i].channel, activeSamples[i].volume)
 #define REPEATS_LEFT(i) activeSamples[i].repeatsLeft
@@ -48,6 +48,7 @@
 #define RESTART_LOOP(i) audio_startLoop(i)
 #define STOP_LOOP(i) Mix_HaltChannel(activeSamples[i].channel)
 
+//Main Sample Structure
 typedef struct {
   Mix_Chunk *sample;
   int channel;
@@ -65,10 +66,12 @@ Sample activeSamples[MAXNUMBEROFSAMPLES] = {{NULL, DEFAULTCHANNEL, false, 1, 0,-
 Sample loadSample(int index);
 void addToActiveArray(int index, Sample sample);
 void setLoopActiveFlag(int index, bool flag);
+
 /* File info */
 void readSampleInfo();
 void tokenizeSampleInfo(char *sampleInfo, char *tokens[]);
 char *createSampleFilePath(char *path);
+
 /* Debug */
 void printTiming(void);
 
@@ -88,8 +91,11 @@ void audio_init(void) {
   readSampleInfo();
 }
 
-/* Loops through activeSamples array once every bar, checks which samples have stopped and retriggers them if they have repeats left, otherwise
-  removes and frees them */
+//-------------------------------------------------------
+
+/* Loops through activeSamples array once every bar, checks which samples
+   have stopped and retriggers them if they have repeats left, otherwise
+   removes and frees them */
 void audio_mainLoop(void) {
   int i;
 
@@ -132,6 +138,8 @@ void audio_mainLoop(void) {
   }
 }
 
+//-------------------------------------------------------
+
 // Add a loop to the activeSamples array to be triggered by the main loop
 void audio_addLoop(int index) {
   Sample sample;
@@ -141,11 +149,6 @@ void audio_addLoop(int index) {
     addToActiveArray(index, sample);
     setLoopActiveFlag(index, true);
   }
-}
-
-// Sets the flag that tells the main loop that the sample should be playing
-void setLoopActiveFlag(int index, bool flag) {
-  activeSamples[index].active = flag;
 }
 
 Sample loadSample(int index) {
@@ -162,19 +165,12 @@ void addToActiveArray(int index, Sample sample) {
   activeSamples[index] = sample;
 }
 
-void audio_close(void) {
-  int i;
-
-  for (i = 0; i < MAXNUMBEROFSAMPLES; ++i) {
-    audio_removeLoop(i);
-      if(sampleFilePaths[i]) {
-        free(sampleFilePaths[i]);
-      }
-  }
-
-  Mix_Quit();
-  SDL_Quit();
+// Sets the flag that tells the main loop that the sample should be playing
+void setLoopActiveFlag(int index, bool flag) {
+  activeSamples[index].active = flag;
 }
+
+//-------------------------------------------------------
 
 void audio_startLoop(int index) {
   activeSamples[index].channel = Mix_PlayChannel(-1, activeSamples[index].sample, activeSamples[index].repeatsLeft);
@@ -184,6 +180,8 @@ void audio_startLoop(int index) {
 
   Mix_Volume(activeSamples[index].channel, activeSamples[index].volume);
 }
+
+//-------------------------------------------------------
 
 void audio_removeLoop(int index) {
   if(Mix_Playing(activeSamples[index].channel)){
@@ -195,10 +193,14 @@ void audio_removeLoop(int index) {
     setLoopActiveFlag(index, false);
 }
 
+//-------------------------------------------------------
+
 // Tells audio main loop that the relevant sample has to be removed
 void audio_markLoopInactive(int index) {
   setLoopActiveFlag(index, false);
 }
+
+//-------------------------------------------------------
 
 void audio_playSampleOnce(int index) {
   Sample buttonSound = {NULL, DEFAULTCHANNEL, false, 1, 0, 1, DEFAULTVOLUME};
@@ -209,6 +211,8 @@ void audio_playSampleOnce(int index) {
     setLoopActiveFlag(MAXNUMBEROFSAMPLES-1, true);
   }
 }
+
+//-------------------------------------------------------
 
 // Stop all samples
 void audio_stop(void) {
@@ -225,7 +229,10 @@ void audio_changeVolume(int index, int volume) {
   activeSamples[index].volume = volume;
 }
 
-// Reads the sample file path and the number of bars into the relevant arrays in the program
+//-------------------------------------------------------
+
+// Reads the sample file path and the number of bars into
+// the relevant arrays in the program
 void readSampleInfo() {
   FILE *sampleInfoFile;
   char sampleInfo[MAXSAMPLEINFOLENGTH];
@@ -273,10 +280,28 @@ void tokenizeSampleInfo(char *sampleInfo, char *tokens[]) {
   }
 }
 
-// Prints the difference between the expected call time of the audio main loop, and the actual call time in ms
+//-------------------------------------------------------
+
+void audio_close(void) {
+  int i;
+
+  for (i = 0; i < MAXNUMBEROFSAMPLES; ++i) {
+    audio_removeLoop(i);
+      if(sampleFilePaths[i]) {
+        free(sampleFilePaths[i]);
+      }
+  }
+
+  Mix_Quit();
+  SDL_Quit();
+}
+
+//-------------------------------------------------------
+
+// Prints the difference between the expected call time of the audio main
+// loop, and the actual call time in ms.
 // Can be switched on by uncommenting the #define DEBUG_TIMING
-void printTiming(void)
-{
+void printTiming(void) {
   newTime = SDL_GetTicks();
   if(newTime != expectedTime){
     time_difference = newTime - expectedTime;
